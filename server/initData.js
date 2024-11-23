9ec49f8b01413487f328e0e5ff366005b34ce71a
Yash Jain <122664937+yjyash@users.noreply.github.com>
2024-11-23 22:54:55 +0530

    Delete server directory

--- server/initData.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-async function main(){
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
-}
-main(); 
